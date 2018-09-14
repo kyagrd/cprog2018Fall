808094c981102a5523c576939502b6a2a7bd0089
Ki Yung Ahn <kyagrd@gmail.com>
2018-09-15 00:29:46 +0900

    some change

--- tools/pull.sh
+++ tools/pull.sh
@@ -3,11 +3,14 @@ GITLABIDS=`cat gitlabids`
 
 for id in $GITLABIDS
 do
-	if [ ! -d "$(id)" ]
+	if [ -d "$id" ]
        	then
-		git clone git@gitlab.com:$id/cprog.git $id ;
+	       	# sem -j+0 --id cprogpull "echo start pull $id; (cd -- $id && git pull); echo end pull $id" &
+	       	echo start pull $id; (cd -- $id && git pull); echo end pull $id
 	else
-		cd $id && git pull
+		# sem -j+0 --id cprogpull "echo start clone $id; git clone git@gitlab.com:$id/cprog.git $id; echo end clone $id" &
+		echo start clone $id; git clone git@gitlab.com:$id/cprog.git $id; echo end clone $id
 	fi
 done
 
+# sem --wait --id cprogpull
