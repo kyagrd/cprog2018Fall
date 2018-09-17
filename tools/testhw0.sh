e146d7f966320450c2bdae82fedea4844d46be34
Ki Yung Ahn <kyagrd@gmail.com>
2018-09-17 11:37:28 +0900

    tst

---
+++ tools/testhw0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+GITLABIDS=`sort gitlabids`
+
+for id in $GITLABIDS
+do
+	if [ -f "$id/hw0/bin/Debug/hw0" ]
+       	then
+		echo ==== testing $id ====
+		(test "`echo 1   | $id/hw0/bin/Debug/hw0`" = 1     && echo 1 || echo 0)  > _hw0$id.txt
+		(test "`echo 5   | $id/hw0/bin/Debug/hw0`" = 25    && echo 1 || echo 0) >> _hw0$id.txt
+		(test "`echo 10  | $id/hw0/bin/Debug/hw0`" = 100   && echo 1 || echo 0) >> _hw0$id.txt
+		(test "`echo 79  | $id/hw0/bin/Debug/hw0`" = 6241  && echo 1 || echo 0) >> _hw0$id.txt
+		(test "`echo 123 | $id/hw0/bin/Debug/hw0`" = 15129 && echo 1 || echo 0) >> _hw0$id.txt
+		echo 0`sed s/^/+/ _hw0$id.txt` | bc
+	fi
+done
