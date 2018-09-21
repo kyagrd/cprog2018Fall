91725814ab414f9c5447aef9d44db316e88bd76f
Ki Yung Ahn <kyagrd@gmail.com>
2018-09-21 16:45:10 +0900

    test script for hw1

--- tools/testhw1.sh
+++ tools/testhw1.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 GITLABIDS=`sort gitlabids`
 
+HW1ANS=kyagrd/hw1/bin/Debug/hw1
+
 for id in $GITLABIDS
 do
 	if [ -f "$id/hw1/bin/Debug/hw1" ]
        	then
-		echo ==== testing $id ====
-		# 아직 채점을 실제로 하지는 않고 hw1 실행파일이 제대로 된 경로에 만들어졌는지만
+		echo ==== testing $id ==== $( \
+			(test "`diff <(echo 1  0  0 | $id/hw1/bin/Debug/hw1) <(echo 1  0  0 | $HW1ANS)`" = ""  && echo 1 || echo 0)  > _hw1$id.txt; \
+			(test "`diff <(echo 1  5  1 | $id/hw1/bin/Debug/hw1) <(echo 1  5  1 | $HW1ANS)`" = ""  && echo 1 || echo 0) >> _hw1$id.txt; \
+			(test "`diff <(echo 1  6  4 | $id/hw1/bin/Debug/hw1) <(echo 1  6  4 | $HW1ANS)`" = ""  && echo 1 || echo 0) >> _hw1$id.txt; \
+			(test "`diff <(echo 1 25 12 | $id/hw1/bin/Debug/hw1) <(echo 1 25 12 | $HW1ANS)`" = ""  && echo 1 || echo 0) >> _hw1$id.txt; \
+			(test "`diff <(echo 2  6  6 | $id/hw1/bin/Debug/hw1) <(echo 2  6  6 | $HW1ANS)`" = ""  && echo 1 || echo 0) >> _hw1$id.txt; \
+			(test "`diff <(echo 3  7  7 | $id/hw1/bin/Debug/hw1) <(echo 3  7  7 | $HW1ANS)`" = ""  && echo 1 || echo 0) >> _hw1$id.txt; \
+			echo 0`sed s/^/+/ _hw1$id.txt` | bc )
+
 	fi
 done
